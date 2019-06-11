f8cbbe2456b05c48f637a337cc81826278396d5a
Daniel <daniellangbehn@gmx.de>
2019-06-11 22:01:28 +0200

    Update index.js

--- Skill Code/lambda/index.js
+++ Skill Code/lambda/index.js
@@ -1,10 +1,12 @@
 const Alexa = require('ask-sdk-core');
 const tb = require('./toolbox.js');
+const strings = require('./strings.json');
 
 let buildIntentHandler = tb.buildIntentHandler;
 let timeLeft = tb.timeLeft;
 let httpGet = tb.httpGet;
 let devieLookup = tb.devieLookup;
+let stringReplace = tb.stringReplace;
 
 // LaunchRequestHandler
 const LaunchRequestHandler = {
@@ -12,165 +14,189 @@ const LaunchRequestHandler = {
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
+        const msg = strings[handlerInput.requestEnvelope.request.locale];
         var speechText = "";
         if (!handlerInput.requestEnvelope.context.System.user.accessToken || handlerInput.requestEnvelope.context.System.user.accessToken.length === 0){
-            speechText = "Willkommen, im Skill Healthy Home Coach. Bitte verbinde diesen Skill mit deinem Netatmo-Account!"
+            speechText = msg.welcome.notLinked;
         return handlerInput.responseBuilder
             .speak(speechText)
             .withLinkAccountCard()
             .getResponse();
         }
-        speechText = "Willkommen, im Skill Healthy Home Coach. Was kann ich für dich tun?";
+        speechText = msg.welcome.linked;
         return handlerInput.responseBuilder
             .speak(speechText)
-            .reprompt("Welche Funktion möchtest du starten?")
+            .reprompt(msg.welcome.reprompt)
             .getResponse();
     }
 };
 
 // Intent Handler
-const GetTemperatureIntentHandler = buildIntentHandler("GetTemperatureIntent", (response, device_num) => {
+const GetTemperatureIntentHandler = buildIntentHandler("GetTemperatureIntent", (response, device_num, locale) => {
     var speechText = "";
     var card = {
-        "title": "'Healthy Home Coach | TestIntent'",
+        "title": strings[locale].temperatureIntent.card.title,
         "message": "",
     };
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
-    const temperature = (response.body.devices[device_num].dashboard_data.Temperature + "").replace(".", ",");
+    let temperature = (response.body.devices[device_num].dashboard_data.Temperature + "");
+    if(locale === "de-DE") temperature = temperature.replace(".", ",");
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = temperature + " °C";
-    card.message = speechText +"\ngemessen vor "+ timeLeft(time_diff);
+    speechText = stringReplace(strings[locale].temperatureIntent.speechText, [{"needle": "{{value}}", "replacement": temperature}]);
+    card.message = stringReplace(strings[locale].temperatureIntent.card.message,
+        [{"needle": "{{speechText}}", "replacement": speechText}, {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
     return {"speechText": speechText, "card": card};
 });
 
-const GetAirQualityIntentHandler = buildIntentHandler("GetAirQualityIntent", (response, device_num) => {
+const GetAirQualityIntentHandler = buildIntentHandler("GetAirQualityIntent", (response, device_num, locale) => {
     let speechText = "";
     let card = {
-        "title": "Healthy Home Coach | CO2-Gehalt",
+        "title": strings[locale].airQualityIntent.card.title,
         "message": "",
     }
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     const co2 = (response.body.devices[device_num].dashboard_data.CO2 + "");
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = co2 + " ppm";
-    card.message = speechText +"\ngemessen vor "+ timeLeft(time_diff);
+    speechText = stringReplace(strings[locale].airQualityIntent.speechText, [{"needle": "{{value}}", "replacement": co2}]);
+    card.message = stringReplace(strings[locale].airQualityIntent.card.message,
+        [{"needle": "{{speechText}}", "replacement": speechText}, {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
     return {"speechText": speechText, "card": card}
 });
 
-const GetHumidityIntentHandler = buildIntentHandler("GetHumidityIntent", (response, device_num) => {
+const GetHumidityIntentHandler = buildIntentHandler("GetHumidityIntent", (response, device_num, locale) => {
     let speechText = "";
     let card = {
-        "title": "Healthy Home Coach | Luftfeuchtigkeit",
+        "title": strings[locale].humidityIntent.card.title,
         "message": "",
     }
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     const humidity = (response.body.devices[device_num].dashboard_data.Humidity + "");
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = humidity + " %";
-    card.message = speechText +"\ngemessen vor "+ timeLeft(time_diff);
+    speechText = stringReplace(strings[locale].humidityIntent.speechText, [{"needle": "{{value}}", "replacement": humidity}]);
+    card.message = stringReplace(strings[locale].humidityIntent.card.message,
+        [{"needle": "{{speechText}}", "replacement": speechText}, {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff)}]);
     return {"speechText": speechText, "card": card};
 });
 
-const GetNoiseIntentHandler = buildIntentHandler("GetNoiseIntent", (response, device_num) => {
+const GetNoiseIntentHandler = buildIntentHandler("GetNoiseIntent", (response, device_num, locale) => {
     let speechText = "";
     let card = {
-        "title": "Healthy Home Coach | Lautstärke",
+        "title": strings[locale].noiseLevelIntent.card.title,
         "message": "",
     }
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     const noise = (response.body.devices[device_num].dashboard_data.Noise + "");
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = noise + " dB";
-    card.message = speechText +"\ngemessen vor "+ timeLeft(time_diff);
+    speechText = stringReplace(strings[locale].noiseLevelIntent.speechText, [{"needle": "{{value}}", "replacement": noise}]);
+    card.message = stringReplace(strings[locale].humidityIntent.card.message,
+        [{"needle": "{{speechText}}", "replacement": speechText}, {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
     return {"speechText": speechText, "card": card};
 });
 
-const GetPressureIntentHandler = buildIntentHandler("GetPressureIntent", (response, device_num) => {
+const GetPressureIntentHandler = buildIntentHandler("GetPressureIntent", (response, device_num, locale) => {
     let speechText = "";
     let card = {
-        "title": "Healthy Home Coach | Luftdruck",
+        "title": strings[locale].airPressureIntent.card.title,
         "message": "",
     }
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
-    const pressure = (response.body.devices[device_num].dashboard_data.Pressure + "").replace(".", ",");
-    const absPressure = (response.body.devices[device_num].dashboard_data.AbsolutePressure + "").replace(".", ",");
+    let pressure = (response.body.devices[device_num].dashboard_data.Pressure + "");
+    let absPressure = (response.body.devices[device_num].dashboard_data.AbsolutePressure + "");
+    if(locale === "de-DE"){
+        pressure = pressure.replace(".", ",");
+        absPressure = absPressure.replace(".", ",");
+    }
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = pressure + " Milli-bar";
-    card.message = pressure + " mbar\nabsolut " + absPressure + " mbar" +"\ngemessen vor "+ timeLeft(time_diff);
+    speechText = stringReplace(strings[locale].airPressureIntent.speechText, [{"needle": "{{value}}", "replacement": pressure}]);
+    card.message = 
+        stringReplace(strings[locale].airPressureIntent.card.message, [{"needle": "{{air_pressure}}", "replacement": pressure}, {"needle": "{{abs_air_pressure}}", "replacement": absPressure}]);
     return {"speechText": speechText, "card": card};
 });
 
-const GetHealthIndexIntentHandler = buildIntentHandler("GetHealthIndexIntent", (response, device_num) => {
+const GetHealthIndexIntentHandler = buildIntentHandler("GetHealthIndexIntent", (response, device_num, locale) => {
     let speechText = "";
     let card = {
-        "title": "Healthy Home Coach | Gesundheitsindex",
+        "title": strings[locale].healthIndexIntent.card.title,
         "message": "",
     }
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     const health_idx = response.body.devices[device_num].dashboard_data.health_idx;
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = ["Gesund", "Gut", "Okay", "Schlecht", "Ungesund"][health_idx];
-    card.message = "Index: " + health_idx + " ("+ speechText +")\ngemessen vor "+ timeLeft(time_diff);
+    speechText = strings[locale].healthIndex[health_idx];
+    card.message =
+        stringReplace(strings[locale].healthIndexIntent.card.message, [
+            {"needle": "{{value}}", "replacement": health_idx},
+            {"needle": "{{spoken_index}}", "replacement": speechText},
+            {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
     return {"speechText": speechText, "card": card};
 });
 
-const GetSumIntentHandler = buildIntentHandler("GetSumIntent", (response, device_num) => {
+const GetSumIntentHandler = buildIntentHandler("GetSumIntent", (response, device_num, locale) => {
     let speechText = "";
     let card = {
-        "title": "Healthy Home Coach",
+        "title": strings[locale].sumIntent.card.title,
         "message": "",
     }
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
-    const data = response.body.devices[device_num].dashboard_data;
+    let data = response.body.devices[device_num].dashboard_data;
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-
-    speechText = "<speak><p>Das Raumklima ist insgesamt " + ["gesund", "gut", "okay", "schlecht", "ungesund"][data.health_idx];
-    speechText += "</p><p>Die Temperatur beträgt " + (data.Temperature + "").replace(".", ",") + " Grad Celsius";
-    speechText += "</p><p>Die Luftfeuchtigkeit beträgt " + data.Humidity + " %";
-    speechText += "</p><p>Der CO2-Gehalt beträgt " + data.CO2 + "ppm";
-    speechText += "</p><p>Die Lautstärke beträgt " + data.Noise + "dB</p></speak>";
-
-    card.message = "Klimaindex: " + data.health_idx + " ("+ ["Gesund", "Gut", "Okay", "Schlecht", "Ungesund"][data.health_idx]+")";
-    card.message += "\nTemperatur: " + (data.Temperature + "").replace(".", ",") + " °C";
-    card.message += "\nLuftfeuchtigkeit: " + data.Humidity + "%";
-    card.message += "\nCO2-Gehalt: " + data.CO2 + " ppm";
-    card.message += "\nLautstärke: " + data.Noise + " dB";
-    card.message += "\ngemessen vor "+ timeLeft(time_diff);
+    
+    speechText = stringReplace(
+        strings[locale].sumIntent.speechText,
+        [{"needle": "{{index}}", "replacement": strings[locale].healthIndex[data.health_idx]},
+        {"needle": "{{temperature}}", "replacement": (locale === "de-DE") ? (data.Temperature+"").replace(".", ",") : data.Temperature},
+        {"needle": "{{humidity}}", "replacement": data.Humidity},
+        {"needle": "{{pressure}}", "replacement": (locale === "de-DE") ? (data.Pressure+"").replace(".", ",") : data.Pressure},
+        {"needle": "{{co2}}", "replacement": data.CO2},
+        {"needle": "{{noise_level}}", "replacement": data.Noise}]);
+    
+    card.message = stringReplace(strings[locale].sumIntent.card.message,
+        [{"needle": "{{index}}", "replacement": data.health_idx},
+        {"needle": "{{spoken_index}}", "replacement": strings[locale].healthIndex[data.health_idx]},
+        {"needle": "{{temperature}}", "replacement": (locale === "de-DE") ? (data.Temperature+"").replace(".", ",") : data.Temperature},
+        {"needle": "{{humidity}}", "replacement": data.Humidity},
+        {"needle": "{{pressure}}", "replacement": (locale === "de-DE") ? (data.Pressure+"").replace(".", ",") : data.Pressure},
+        {"needle": "{{co2}}", "replacement": data.CO2},
+        {"needle": "{{noise_level}}", "replacement": data.Noise},
+        {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
+        
     return {"speechText": speechText, "card": card};
 });
 
-const GetLastUpdateIntentHandler = buildIntentHandler("GetLastUpdateIntent", (response, device_num) => {
+const GetLastUpdateIntentHandler = buildIntentHandler("GetLastUpdateIntent", (response, device_num, locale) => {
     let speechText = "";
     let card = {
-        "title": "Healthy Home Coach | Messzeitpunkt",
+        "title": strings[locale].timeIntent.card.title,
         "message": "",
     }
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = "Messung durchgeführt vor " + timeLeft(time_diff);
+    speechText = stringReplace(strings[locale].timeIntent.speechText, [{"needle": "{{value}}", "replacement": timeLeft(time_diff, locale)}]);
     card.message = speechText;
     return {"speechText": speechText, "card": card};
 });
 
-const GetDeviceLocationIntentHandler = buildIntentHandler("GetDeviceLocationIntent", (response, device_num) => {
+const GetDeviceLocationIntentHandler = buildIntentHandler("GetDeviceLocationIntent", (response, device_num, locale) => {
     let speechText = "";
     let card = {
-        "title": "Healthy Home Coach | Standort",
+        "title": strings[locale].locationIntent.card.title,
         "message": "",
     }
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     const place = response.body.devices[device_num].place;
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
     speechText = place.city;
-    card.message = speechText + "\n\tLong: "+place.location[0];
-    card.message += "\n\tLang: " + place.location[1];
+    card.message = stringReplace(strings[locale].locationIntent.card.message,
+        [{"needle": "{{city}}", "replacement": place.city},
+        {"needle": "{{lat}}", "replacement": place.location[0]},
+        {"needle": "{{long}}", "replacement": place.location[1]}]);
     return {"speechText": speechText, "card": card};
 });
 
-const GetUserEmailIntentHandler = buildIntentHandler("GetUserEmailIntent", (response, device_num) => {
+const GetUserEmailIntentHandler = buildIntentHandler("GetUserEmailIntent", (response, device_num, locale) => {
     let speechText = "";
     let card = {
-        "title": "Healthy Home Coach | Emailaddresse",
+        "title": strings[locale].mailIntent.card.title,
         "message": "",
     }
     const mail = response.body.user.mail;
@@ -179,31 +205,31 @@ const GetUserEmailIntentHandler = buildIntentHandler("GetUserEmailIntent", (resp
     return {"speechText": speechText, "card": card};
 });
 
-const GetWifiStatusIntentHandler = buildIntentHandler("GetWifiStatusIntent", (response, device_num) => {
+const GetWifiStatusIntentHandler = buildIntentHandler("GetWifiStatusIntent", (response, device_num, locale) => {
     let speechText = "";
     let card = {
-        "title": "Healthy Home Coach | WiFi-Status",
+        "title": strings[locale].wifiSignalIntent.card.title,
         "message": "",
     }
     const wifi_status = response.body.devices[device_num].wifi_status;
     speechText = wifi_status+"";
-    card.message = speechText + "\n(86=schlecht, 56=gut)";
+    card.message = stringReplace(strings[locale].wifiSignalIntent.card.message, [{"needle": "{{value}}", "replacement": wifi_status}]);
     return {"speechText": speechText, "card": card};
 });
 
-const GetDeviceListIntentHandler = buildIntentHandler("GetDeviceListIntent", (response, device_num) => {
+const GetDeviceListIntentHandler = buildIntentHandler("GetDeviceListIntent", (response, device_num, locale) => {
     let speechText = "";
     let card = {
-        "title": "Healthy Home Coach | DeviceList",
+        "title": strings[locale].deviceListIntent.card.title,
         "message": "",
     }
     const count = response.body.devices.length;
     const devices = response.body.devices;
     if (count >= 2){
-        speechText = "<p>" + count + " Geräte sind verknüpft.</p>";
+        speechText = stringReplace(strings[locale].deviceListIntent.speechText, [{"needle": "{{count}}", "replacement": count}]);
     }
     devices.forEach((it) => {
-        speechText += "<p>Gerät: "+it.station_name+"</p>";
+        speechText += stringReplace(strings[locale].deviceListIntent.stationName, [{"needle": "{{station_name}}", "replacement": count}]);
         card.message += it.station_name + " ("+it._id+")\n";
     });
     return {"speechText": speechText, "card": card};
@@ -216,7 +242,7 @@ const HelpIntentHandler = {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest' && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-        const speechText = 'Du kannst mich nach dem Raumklima fragen. Wie kann ich dir helfen?';
+        const speechText = strings[handlerInput.requestEnvelope.request.locale].helpIntent;
         return handlerInput.responseBuilder.speak(speechText).reprompt(speechText).getResponse();
     }
 };
@@ -228,7 +254,7 @@ const CancelAndStopIntentHandler = {
                 || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
     },
     handle(handlerInput) {
-        const speechText = 'bis bald!';
+        const speechText = strings[handlerInput.requestEnvelope.request.locale].stopIntent;
         return handlerInput.responseBuilder.speak(speechText).getResponse();
     }
 };
@@ -251,7 +277,7 @@ const IntentReflectorHandler = {
         const intentName = handlerInput.requestEnvelope.request.intent.name;
         
          // const speechText = `You just triggered ${intentName}`;
-         const speechText = "<speak><audio src='soundbank://soundlibrary/musical/amzn_sfx_electronic_beep_02'/></speak>"
+         const speechText = strings[handlerInput.requestEnvelope.request.locale].unhandledIntent;
 
         return handlerInput.responseBuilder.speak(speechText).getResponse();
     }
@@ -263,7 +289,7 @@ const ErrorHandler = {
     },
     handle(handlerInput, error) {
         console.log(`~~~~ Error handled: ${error.message}`);
-        const speechText = `Entschuldigung, das habe ich nicht verstanden. Bitte versuche es noch einmal!`;
+        const speechText = strings[handlerInput.requestEnvelope.request.locale].errorHandler;
         return handlerInput.responseBuilder.speak(speechText).reprompt(speechText).getResponse();
     }
 };
