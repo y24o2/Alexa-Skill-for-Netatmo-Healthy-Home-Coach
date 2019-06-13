212d910dc00f04e72cd76614b2ce83b2bc4fd8fb
Daniel <daniellangbehn@gmx.de>
2019-06-13 21:01:18 +0200

    Update index.js

--- Skill Code/lambda/index.js
+++ Skill Code/lambda/index.js
@@ -38,13 +38,15 @@ const GetTemperatureIntentHandler = buildIntentHandler("GetTemperatureIntent", (
         "title": strings[locale].temperatureIntent.card.title,
         "message": "",
     };
+    const station_name = response.body.devices[device_num].station_name;
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     let temperature = (response.body.devices[device_num].dashboard_data.Temperature + "");
     if(locale === "de-DE") temperature = temperature.replace(".", ",");
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = stringReplace(strings[locale].temperatureIntent.speechText, [{"needle": "{{value}}", "replacement": temperature}]);
+    speechText = stringReplace(strings[locale].temperatureIntent.speechText, [{
+        "needle": "{{value}}", "replacement": temperature}, {"needle": "{{stationName}}", "replacement": station_name}]);
     card.message = stringReplace(strings[locale].temperatureIntent.card.message,
-        [{"needle": "{{speechText}}", "replacement": speechText}, {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
+        [{"needle": "{{value}}", "replacement": temperature}, {"needle": "{{stationName}}", "replacement": station_name}, {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
     return {"speechText": speechText, "card": card};
 });
 
@@ -54,12 +56,13 @@ const GetAirQualityIntentHandler = buildIntentHandler("GetAirQualityIntent", (re
         "title": strings[locale].airQualityIntent.card.title,
         "message": "",
     }
+    const station_name = response.body.devices[device_num].station_name;
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     const co2 = (response.body.devices[device_num].dashboard_data.CO2 + "");
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = stringReplace(strings[locale].airQualityIntent.speechText, [{"needle": "{{value}}", "replacement": co2}]);
+    speechText = stringReplace(strings[locale].airQualityIntent.speechText, [{"needle": "{{value}}", "replacement": co2}, {"needle": "{{stationName}}", "replacement": station_name}]);
     card.message = stringReplace(strings[locale].airQualityIntent.card.message,
-        [{"needle": "{{speechText}}", "replacement": speechText}, {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
+        [{"needle": "{{value}}", "replacement": co2}, {"needle": "{{stationName}}", "replacement": station_name}, {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
     return {"speechText": speechText, "card": card}
 });
 
@@ -69,12 +72,13 @@ const GetHumidityIntentHandler = buildIntentHandler("GetHumidityIntent", (respon
         "title": strings[locale].humidityIntent.card.title,
         "message": "",
     }
+    const station_name = response.body.devices[device_num].station_name;
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     const humidity = (response.body.devices[device_num].dashboard_data.Humidity + "");
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = stringReplace(strings[locale].humidityIntent.speechText, [{"needle": "{{value}}", "replacement": humidity}]);
+    speechText = stringReplace(strings[locale].humidityIntent.speechText, [{"needle": "{{value}}", "replacement": humidity}, {"needle": "{{stationName}}", "replacement": station_name}]);
     card.message = stringReplace(strings[locale].humidityIntent.card.message,
-        [{"needle": "{{speechText}}", "replacement": speechText}, {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
+        [{"needle": "{{value}}", "replacement": humidity}, {"needle": "{{stationName}}", "replacement": station_name}, {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
     return {"speechText": speechText, "card": card};
 });
 
@@ -84,12 +88,13 @@ const GetNoiseIntentHandler = buildIntentHandler("GetNoiseIntent", (response, de
         "title": strings[locale].noiseLevelIntent.card.title,
         "message": "",
     }
+    const station_name = response.body.devices[device_num].station_name;
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     const noise = (response.body.devices[device_num].dashboard_data.Noise + "");
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = stringReplace(strings[locale].noiseLevelIntent.speechText, [{"needle": "{{value}}", "replacement": noise}]);
-    card.message = stringReplace(strings[locale].humidityIntent.card.message,
-        [{"needle": "{{speechText}}", "replacement": speechText}, {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
+    speechText = stringReplace(strings[locale].noiseLevelIntent.speechText, [{"needle": "{{value}}", "replacement": noise}, {"needle": "{{stationName}}", "replacement": station_name}]);
+    card.message = stringReplace(strings[locale].noiseLevelIntent.card.message,
+        [{"needle": "{{value}}", "replacement": noise}, {"needle": "{{stationName}}", "replacement": station_name}, {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
     return {"speechText": speechText, "card": card};
 });
 
@@ -99,6 +104,7 @@ const GetPressureIntentHandler = buildIntentHandler("GetPressureIntent", (respon
         "title": strings[locale].airPressureIntent.card.title,
         "message": "",
     }
+    const station_name = response.body.devices[device_num].station_name;
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     let pressure = (response.body.devices[device_num].dashboard_data.Pressure + "");
     let absPressure = (response.body.devices[device_num].dashboard_data.AbsolutePressure + "");
@@ -107,9 +113,13 @@ const GetPressureIntentHandler = buildIntentHandler("GetPressureIntent", (respon
         absPressure = absPressure.replace(".", ",");
     }
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = stringReplace(strings[locale].airPressureIntent.speechText, [{"needle": "{{value}}", "replacement": pressure}]);
+    speechText = stringReplace(strings[locale].airPressureIntent.speechText, [{"needle": "{{value}}", "replacement": pressure}, {"needle": "{{stationName}}", "replacement": station_name}]);
     card.message = 
-        stringReplace(strings[locale].airPressureIntent.card.message, [{"needle": "{{air_pressure}}", "replacement": pressure}, {"needle": "{{abs_air_pressure}}", "replacement": absPressure}]);
+        stringReplace(strings[locale].airPressureIntent.card.message, [
+            {"needle": "{{air_pressure}}", "replacement": pressure},
+            {"needle": "{{abs_air_pressure}}", "replacement": absPressure},
+            {"needle": "{{stationName}}", "replacement": station_name},
+            {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
     return {"speechText": speechText, "card": card};
 });
 
@@ -119,14 +129,17 @@ const GetHealthIndexIntentHandler = buildIntentHandler("GetHealthIndexIntent", (
         "title": strings[locale].healthIndexIntent.card.title,
         "message": "",
     }
+    const station_name = response.body.devices[device_num].station_name;
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     const health_idx = response.body.devices[device_num].dashboard_data.health_idx;
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = strings[locale].healthIndex[health_idx];
+    let value = strings[locale].healthIndex[health_idx];
+    speechText = stringReplace(strings[locale].healthIndexIntent.speechText, [{"needle": "{{value}}", "replacement": value}, {"needle": "{{stationName}}", "replacement": station_name}]);
     card.message =
         stringReplace(strings[locale].healthIndexIntent.card.message, [
             {"needle": "{{value}}", "replacement": health_idx},
-            {"needle": "{{spoken_index}}", "replacement": speechText},
+            {"needle": "{{spoken_index}}", "replacement": value},
+            {"needle": "{{stationName}}", "replacement": station_name},
             {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
     return {"speechText": speechText, "card": card};
 });
@@ -137,6 +150,7 @@ const GetSumIntentHandler = buildIntentHandler("GetSumIntent", (response, device
         "title": strings[locale].sumIntent.card.title,
         "message": "",
     }
+    const station_name = response.body.devices[device_num].station_name;
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     let data = response.body.devices[device_num].dashboard_data;
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
@@ -148,7 +162,8 @@ const GetSumIntentHandler = buildIntentHandler("GetSumIntent", (response, device
         {"needle": "{{humidity}}", "replacement": data.Humidity},
         {"needle": "{{pressure}}", "replacement": (locale === "de-DE") ? (data.Pressure+"").replace(".", ",") : data.Pressure},
         {"needle": "{{co2}}", "replacement": data.CO2},
-        {"needle": "{{noise_level}}", "replacement": data.Noise}]);
+        {"needle": "{{noise_level}}", "replacement": data.Noise},
+        {"needle": "{{stationName}}", "replacement": station_name}]);
     
     card.message = stringReplace(strings[locale].sumIntent.card.message,
         [{"needle": "{{index}}", "replacement": data.health_idx},
@@ -158,6 +173,7 @@ const GetSumIntentHandler = buildIntentHandler("GetSumIntent", (response, device
         {"needle": "{{pressure}}", "replacement": (locale === "de-DE") ? (data.Pressure+"").replace(".", ",") : data.Pressure},
         {"needle": "{{co2}}", "replacement": data.CO2},
         {"needle": "{{noise_level}}", "replacement": data.Noise},
+        {"needle": "{{stationName}}", "replacement": station_name},
         {"needle": "{{timeLeft}}", "replacement": timeLeft(time_diff, locale)}]);
         
     return {"speechText": speechText, "card": card};
@@ -169,10 +185,13 @@ const GetLastUpdateIntentHandler = buildIntentHandler("GetLastUpdateIntent", (re
         "title": strings[locale].timeIntent.card.title,
         "message": "",
     }
+    const station_name = response.body.devices[device_num].station_name;
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = stringReplace(strings[locale].timeIntent.speechText, [{"needle": "{{value}}", "replacement": timeLeft(time_diff, locale)}]);
-    card.message = speechText;
+    speechText = stringReplace(strings[locale].timeIntent.speechText, [{"needle": "{{stationName}}", "replacement": station_name},
+        {"needle": "{{value}}", "replacement": timeLeft(time_diff, locale)}]);
+    card.message = stringReplace(strings[locale].timeIntent.card.message, [{"needle": "{{stationName}}", "replacement": station_name},
+        {"needle": "{{value}}", "replacement": timeLeft(time_diff, locale)}]);
     return {"speechText": speechText, "card": card};
 });
 
@@ -182,10 +201,11 @@ const GetDeviceLocationIntentHandler = buildIntentHandler("GetDeviceLocationInte
         "title": strings[locale].locationIntent.card.title,
         "message": "",
     }
+    const station_name = response.body.devices[device_num].station_name;
     const time_utc = response.body.devices[device_num].dashboard_data.time_utc;
     const place = response.body.devices[device_num].place;
     let time_diff = Math.floor(Date.now() / 1000) - time_utc;
-    speechText = place.city;
+    speechText = stringReplace(strings[locale].locationIntent.speechText, [{"needle": "{{city}}", "replacement": place.city}, {"needle": "{{stationName}}", "replacement": station_name}]);
     card.message = stringReplace(strings[locale].locationIntent.card.message,
         [{"needle": "{{city}}", "replacement": place.city},
         {"needle": "{{lat}}", "replacement": place.location[0]},
@@ -200,8 +220,8 @@ const GetUserEmailIntentHandler = buildIntentHandler("GetUserEmailIntent", (resp
         "message": "",
     }
     const mail = response.body.user.mail;
-    speechText = mail;
-    card.message = speechText;
+    speechText = stringReplace(strings[locale].mailIntent.speechText, [{"needle": "{{mail}}", "replacement": mail}]);
+    card.message = stringReplace(strings[locale].mailIntent.card.message, [{"needle": "{{mail}}", "replacement": mail}]);
     return {"speechText": speechText, "card": card};
 });
 
@@ -211,6 +231,7 @@ const GetWifiStatusIntentHandler = buildIntentHandler("GetWifiStatusIntent", (re
         "title": strings[locale].wifiSignalIntent.card.title,
         "message": "",
     }
+    const station_name = response.body.devices[device_num].station_name;
     const wifi_status = response.body.devices[device_num].wifi_status;
     speechText = wifi_status+"";
     card.message = stringReplace(strings[locale].wifiSignalIntent.card.message, [{"needle": "{{value}}", "replacement": wifi_status}]);
@@ -229,7 +250,7 @@ const GetDeviceListIntentHandler = buildIntentHandler("GetDeviceListIntent", (re
         speechText = stringReplace(strings[locale].deviceListIntent.speechText, [{"needle": "{{count}}", "replacement": count}]);
     }
     devices.forEach((it) => {
-        speechText += stringReplace(strings[locale].deviceListIntent.stationName, [{"needle": "{{station_name}}", "replacement": count}]);
+        speechText += stringReplace(strings[locale].deviceListIntent.stationName, [{"needle": "{{stationName}}", "replacement": it.station_name}]);
         card.message += it.station_name + " ("+it._id+")\n";
     });
     return {"speechText": speechText, "card": card};
